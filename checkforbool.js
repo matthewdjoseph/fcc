b9dc1ebb5d268c0ecf60522350971859b332b525
matthewdjoseph <matthewjoseph.student@careerdevs.com>
2019-09-28 13:35:07 -0400

    check if argument is boolean

---
+++ checkforbool.js
@@ -0,0 +1,10 @@
+function booWho(bool) {
+    // What is the new fad diet for ghost developers? The Boolean.
+    if(bool === true || bool === false){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  
+  console.log(booWho(null));
